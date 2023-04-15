f83b3a1b1e36353a2825da95aca2cddefb8b9560
Ayush Jain <ayushganna67@gmail.com>
2023-04-16 03:10:16 +0530

    Fix mobile view

--- NITK-FRONTEND/src/components/SubjectBox.jsx
+++ NITK-FRONTEND/src/components/SubjectBox.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Flex,Box,Text,Image, Stack, HStack} from '@chakra-ui/react'
+import {Flex,Box,Text,Image, Stack, HStack,Container} from '@chakra-ui/react'
 
 const SubjectsBox = () => {
     return ( 
         <Flex direction="column" width="100%" padding={{base:'5px',md:"120px"}}>
             <Stack direction={{base:'column',md:'row'}} spacing={{base:"10px",md:"30px"}}>
+                <Box maxW={"100%"}>    
                 <HStack maxW={{base:"100%",md:"50%"}} spacing="0" paddingX={{base:"10px",md:"none"}}>
                 <Image  src={
               'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
@@ -26,13 +27,15 @@ const SubjectsBox = () => {
 Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.
                 </Text>
                 </HStack>
-                                <HStack maxW={{base:"100%",md:"50%"}} spacing="0" paddingX={{base:"10px",md:"none"}}>
+                </Box>
+                <Box maxW="100%">          
+                <HStack maxW={{base:"100%",md:"50%"}} spacing="0" paddingX={{base:"10px",md:"none"}}>
                 <Image  src={
               'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
             }
             borderRadius="15px">
                 </Image>
-                <Text margin="0" padding="15px" border="1px" display="flex" flexWrap="wrap">
+                <Text margin="0" padding="15px" border="1px" display="flex" flexWrap="wrap" >
                  History. History is now with Future.Tap to read more.
                 </Text>
                 </HStack>
@@ -47,6 +50,7 @@ Caffè latte is a coffee beverage of Italian origin made with espresso and steam
 Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.
                 </Text>
                 </HStack>
+                </Box>
             </Stack>
             
         </Flex>
--- NITK-FRONTEND/src/pages/LandingPage.jsx
+++ NITK-FRONTEND/src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar,Whatisthis,Subjects,Dhabawale} from '../components'
+import {Navbar,Whatisthis,SubjectsBox, Subjects,Dhabawale} from '../components'
 
 const LandingPage = () => {
     return ( 
@@ -7,7 +7,9 @@ const LandingPage = () => {
             <Navbar />
             <Whatisthis />
             <Subjects />
+            <SubjectsBox/>
             <Dhabawale />
+            
         </div>
      );
 }
