12b2c5f0b9011a601097c6ca0d367e49f20d9499
Aman <amanclementaranha@gmail.com>
2023-04-16 03:09:02 +0530

    subjectList

--- NITK-FRONTEND/src/components/SubjectBox.jsx
+++ NITK-FRONTEND/src/components/SubjectBox.jsx
@@ -1,52 +1,54 @@
 import React from 'react';
-import {Flex,Box,Text,Image, Stack, HStack} from '@chakra-ui/react'
+import {Flex,Box,Text,Image, Stack, HStack, VStack, Heading} from '@chakra-ui/react'
 
 const SubjectsBox = () => {
     return ( 
         <Flex direction="column" width="100%" padding={{base:'5px',md:"120px"}}>
-            <Stack direction={{base:'column',md:'row'}} spacing={{base:"10px",md:"30px"}}>
-                <HStack maxW={{base:"100%",md:"50%"}} spacing="0" paddingX={{base:"10px",md:"none"}}>
+            <Stack direction="column" spacing={{base:"10px",md:"30px"}}>
+                <Flex direction={{base:"column",md:"row"}}>                
+                <HStack maxW={{base:"100%",md:"50%"}} spacing="0" paddingX={{base:"10px",md:"none"}} paddingY={{base:"5px"}}>
                 <Image  src={
               'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
             }
             borderRadius="15px">
                 </Image>
                 <Text margin="0" padding="15px" border="1px" display="flex" flexWrap="wrap">
-                 History. History is now with Future.Tap to read more.
+                 Biology: The study of living organisms and their interactions with the environment.
                 </Text>
                 </HStack>
-                <HStack maxW={{base:"100%",md:"50%"}} spacing="0">
-                <Image  src={
-              'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
-            }
-            borderRadius="15px">
-                </Image>
-                <Text margin="0" padding="15px" border="1px" >
-                 The perfect latte
-Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.
-                </Text>
-                </HStack>
-                                <HStack maxW={{base:"100%",md:"50%"}} spacing="0" paddingX={{base:"10px",md:"none"}}>
+               <HStack maxW={{base:"100%",md:"50%"}} spacing="0" paddingX={{base:"10px",md:"none"}} paddingY={{base:"5px"}}>
                 <Image  src={
               'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
             }
             borderRadius="15px">
                 </Image>
                 <Text margin="0" padding="15px" border="1px" display="flex" flexWrap="wrap">
-                 History. History is now with Future.Tap to read more.
+                Physics: The study of matter, energy, and their interactions in the universe.
                 </Text>
                 </HStack>
-                <HStack maxW={{base:"100%",md:"50%"}} spacing="0">
+                </Flex>
+                <Flex direction={{base:"column",md:"row"}}>
+                <HStack maxW={{base:"100%",md:"50%"}} spacing="0" paddingX={{base:"10px",md:"none"}} paddingY={{base:"5px"}}>
+                <Image  src={
+              'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
+            }
+            borderRadius="15px">
+                </Image>
+                <Text margin="0" padding="15px" border="1px" display="flex" flexWrap="wrap">
+                 Chemistry: The study of the composition, structure, properties, and reactions of matter.
+                </Text>
+                </HStack>
+                <HStack maxW={{base:"100%",md:"50%"}} spacing="0" paddingX={{base:"10px",md:"none"}} paddingY={{base:"5px"}}>
                 <Image  src={
               'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
             }
             borderRadius="15px">
                 </Image>
                 <Text margin="0" padding="15px" border="1px" >
-                 The perfect latte
-Caffè latte is a coffee beverage of Italian origin made with espresso and steamed milk.
+                    History: The study of past events and their impact on societies and individuals.
                 </Text>
                 </HStack>
+                </Flex>
             </Stack>
             
         </Flex>
