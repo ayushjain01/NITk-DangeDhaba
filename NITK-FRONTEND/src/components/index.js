84ce520c0f10b8e30b93c4f6efccaaa9d6572b57
Aman <amanclementaranha@gmail.com>
2023-04-16 03:55:17 +0530

    footer

--- NITK-FRONTEND/src/components/index.js
+++ NITK-FRONTEND/src/components/index.js
@@ -3,6 +3,7 @@ import Whatisthis from "./Whatisthis";
 import Subjects from "./SubjectsList";
 import SubjectsBox from "./SubjectBox";
 import Dhabawale from "./Dhabawale";
+import Footer from "./Footer";
 
 
-export {Navbar,Subjects,SubjectsBox,Dhabawale,Whatisthis}
+export {Navbar,Subjects,SubjectsBox,Dhabawale,Whatisthis,Footer}
