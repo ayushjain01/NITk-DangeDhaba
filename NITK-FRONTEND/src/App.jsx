ed3983fefd8f649b6887b57c2a96a20ede4688cf
Aman <amanclementaranha@gmail.com>
2023-04-15 17:51:14 +0530

    navbar

--- NITK-FRONTEND/src/App.jsx
+++ NITK-FRONTEND/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import {Routes,Route} from 'react-router-dom'
+import { LandingPage } from './pages'
 
 function App() {
   return (
---
+++ NITK-FRONTEND/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Navbar} from '../components'
+
+const LandingPage = () => {
+    return ( 
+        <div className="LandingPage">
+            <Navbar />
+        </div>
+     );
+}
+ 
+export default LandingPage;
